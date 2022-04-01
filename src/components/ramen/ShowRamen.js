fa2b49ae44e728bc60e378df7e462e0183896f6c
NikPurvis <nikpurvis@gmail.com>
2022-03-31 21:16:13 -0600

    Show views are pulling and displaying info for individual ramen

--- src/components/ramen/ShowRamen.js
+++ src/components/ramen/ShowRamen.js
@@ -2,14 +2,45 @@
 import React, { useState, useEffect } from "react"
 import { getOneRamen } from "../../api/ramen"
 import { useParams } from "react-router-dom"
+import { Spinner, Container, Card } from "react-bootstrap"
 
 const ShowRamen = (props) => {
+    const [ramen, setRamen] = useState(null)
     console.log("props in showRamen:", props)
-    const {params} = useParams()
-    console.log("params in showRamen:", params)
+    const { id } = useParams()
+    console.log("id in showRamen:", id)
+
+    useEffect(() => {
+        getOneRamen(id)
+        .then(res => setRamen(res.data.ramen))
+            .catch(console.error)
+    }, [id])
+
+    if (!ramen) {
+        return (
+            <Container fluid className="justify-content-center">
+                <Spinner animation="border" role="status" variant="primary" >
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </Container>
+        )
+    }
 
     return (
-        <p>Show ramen component</p>
+        // <p>here's some {ramen.flavor}</p>
+
+        <Container className="fluid">
+            <Card>
+                <Card.Header>{ramen.flavor}</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        {ramen.description}<br/>
+                        {ramen.sodium}mg sodium per packet<br/>
+                        Have you tried it? {ramen.haveTried ? "OH YEAH" : "No, and I must!"}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Container>
     )
 }
 
